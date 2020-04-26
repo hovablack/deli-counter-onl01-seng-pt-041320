840fec40a300ef09c0d1ec5331163e5ea29f7fae
Osi Ejedawe <osi.ejedawe@hotmail.com>
2020-04-26 23:35:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,24 +17,10 @@ def take_a_number(deli, name)
 end
 
 def now_serving(deli)
-  if deli.length == 0
   if deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{deli.first}."
     deli.shift
-  end
-end
-
-def line(deli)
-  if deli.length == 0
-    puts "The line is currently empty."
-  else
-    current_line = "The line is currently:"
-    deli.each_with_index do |person, i|
-      current_line << " #{i+1}. #{person}"
-    end
-
-    puts current_line
   end
 end
